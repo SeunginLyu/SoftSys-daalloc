0bba1d2eed936ce06d061c1de294d6397ceaa8cd
SeunginLyu <seungin.lyu@students.olin.edu>
2019-03-15 12:38:21 -0400

    shchgrp compares regexes against std in

--- src/shchgrp.c
+++ src/shchgrp.c
@@ -33,6 +33,8 @@ int arg_parse(int argc, char* argv[]){
 int main(int argc, char *argv[]){
     int num_regexes = 0;
     Regex ** regexes;
+    int * matches;
+
     if(arg_parse(argc, argv)){
         return 1;
     } else {
@@ -44,18 +46,32 @@ int main(int argc, char *argv[]){
         }
         
         regexes = (Regex **) malloc(sizeof(Regex*) * num_regexes);
-        
+        matches = malloc(sizeof(int) * num_regexes);
+
         for(int index = 0; index < num_regexes; index++){
             regexes[index] = make_regex(argv[index + optind], REG_EXTENDED | REG_NOSUB);
+            matches[index] = shvec_create();
         }
     }
 
     printf("Regexes:\n");
     for(int i = 0; i < num_regexes; i++){
-        if(regexec(regexes[i], "hello world", 0, NULL, 0)) {
+        int match = regexec(regexes[i], "hello world", 0, NULL, 0);
+        if(match == 0) {
             printf("regex number %d matches hello world\n", i);
         }
     }
+
+    char in_buf[100];
+    while(fgets(in_buf, 100, stdin) != NULL) {
+        for(int i = 0; i < num_regexes; i++){
+            int match = regexec(regexes[i], in_buf, 0, NULL, 0);
+            if(match == 0) {
+                printf("regex number %d matches %s\n", i, in_buf);
+            }
+        }   
+    }
+    
     // Run these against the stdin
     return 0;
 }
